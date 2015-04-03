d216ae553b777fe957219746abf943a1e9879ee4
ECP <ECP@Crystal_Paladin>
2015-04-04 06:50:35 +0700

    Ringchart data masuk

--- src/monitoringentry/chart/UploadedDataChart.java
+++ src/monitoringentry/chart/UploadedDataChart.java
@@ -7,23 +7,29 @@ package monitoringentry.chart;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.text.DecimalFormat;
+import java.awt.Font;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.geom.Rectangle2D;
 import javax.swing.JPanel;
 import monitoringentry.dao.B1Dao;
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.labels.PieSectionLabelGenerator;
-import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
-import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.plot.PiePlotState;
+import org.jfree.chart.plot.RingPlot;
+import org.jfree.chart.title.TextTitle;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
+import org.jfree.text.TextUtilities;
+import org.jfree.ui.HorizontalAlignment;
+import org.jfree.ui.TextAnchor;
 
 /**
  *
- * @author Habib
+ * @author gal
  */
-public class UploadedDataChart extends JPanel{
+public class UploadedDataChart extends JPanel {
 
     public UploadedDataChart() {
         PieDataset dataset = createDataset();
@@ -35,26 +41,79 @@ public class UploadedDataChart extends JPanel{
 
     private PieDataset createDataset() {
         DefaultPieDataset dataset = new DefaultPieDataset();
-        long data = B1Dao.getInstance().getRowCount();
-        dataset.setValue("Data Masuk", B1Dao.getInstance().getRowCount());
-        dataset.setValue("Belum dientry", (2000-data));
+        long data = B1Dao.getInstance().getRowCount()+212;
+        dataset.setValue("Data Masuk", data);
+        dataset.setValue("Belum dientry", (1888 - data));
         return dataset;
     }
 
-    private JFreeChart createChart(PieDataset dataset) {
-        JFreeChart chart = ChartFactory.createPieChart("Summary Data Masuk", dataset, true, true, false);
-        chart.removeLegend();
-        PiePlot plot = (PiePlot) chart.getPlot();
-        plot.setSectionPaint("Data Masuk", Color.BLUE);
-        plot.setSectionPaint("Belum dientry", Color.red);
-        plot.setExplodePercent("Data Masuk", 0.10);
-        plot.setSimpleLabels(true);
+    static class CustomRingPlot extends RingPlot {
 
-        PieSectionLabelGenerator gen = new StandardPieSectionLabelGenerator(
-            "{0}: {1} ({2})", new DecimalFormat("0"), new DecimalFormat("0%"));
-        plot.setLabelGenerator(gen);
-        return chart;
+        private final Font centerTextFont;
+        private final Color centerTextColor;
+
+        public CustomRingPlot(PieDataset dataset) {
+            super(dataset);
+            this.centerTextFont = new Font(Font.SANS_SERIF, Font.BOLD, 24);
+            this.centerTextColor = Color.LIGHT_GRAY;
+        }
+
+        @Override
+        protected void drawItem(Graphics2D g2, int section,
+                Rectangle2D dataArea, PiePlotState state, int currentPass) {
+            super.drawItem(g2, section, dataArea, state, currentPass);
+            if (currentPass == 1 && section == 0) {
+                Number n = Math.round((this.getDataset().getValue(section)).doubleValue());
+                String string = n.toString();
+                double dob = (Double.parseDouble(string)) * 100 / 1888;
+                String persen = null;
+                if (dob <= 100) {
+                    persen = String.format("%.2f%%", dob).replace(".", ",");
+                } else {
+                    persen = "Overload";
+                }
+                g2.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 17));
+                g2.setPaint(Color.green);
+                TextUtilities.drawAlignedString(persen, g2,
+                        (float) dataArea.getCenterX(),
+                        (float) dataArea.getCenterY(),
+                        TextAnchor.CENTER);
+
+                g2.setFont(this.centerTextFont);
+                g2.setPaint(this.centerTextColor);
+                TextUtilities.drawAlignedString(string, g2,
+                        (float) dataArea.getCenterX(),
+                        (float) dataArea.getCenterY() - 5,
+                        TextAnchor.BOTTOM_CENTER);
+
+            }
+        }
+
     }
-    
-    
+
+    private static JFreeChart createChart(PieDataset dataset) {
+        CustomRingPlot plot = new CustomRingPlot(dataset);
+        JFreeChart chart = new JFreeChart("Jumlah Data masuk",
+                JFreeChart.DEFAULT_TITLE_FONT, plot, false);
+        chart.setBackgroundPaint(new GradientPaint(new Point(0, 0),
+                Color.DARK_GRAY, new Point(400, 200), Color.blue));
+
+        TextTitle t = chart.getTitle();
+        t.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        t.setPaint(new Color(240, 240, 240));
+        t.setFont(new Font("Arial", Font.BOLD, 24));
+
+        plot.setBackgroundPaint(null);
+        plot.setOutlineVisible(false);
+        plot.setLabelGenerator(null);
+        plot.setSectionPaint("Data Masuk", Color.white);
+        plot.setSectionPaint("Belum dientry", new Color(100, 100, 100));
+        plot.setSectionDepth(0.12);
+        plot.setSectionOutlinesVisible(false);
+        plot.setShadowPaint(null);
+
+        return chart;
+
+    }
+
 }
